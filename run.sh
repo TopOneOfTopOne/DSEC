6229be3a3aca1e317d0e7857131d7ce57dba63b0
toponeoftopone <nooblingis@gmail.com>
2016-02-11 14:47:45 +0800

    added bash script to run with double click

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#ls
+#Need to change permissions when first created $chmod 755 run.sh
+#To run on double click:
+# chmod a+x yourscriptname
+# change the default application to terminal in finder
+
+ruby Desktop/DSEC/terminal.rb
